adadab050580211754740a6b1c7066f0dcf718f6
hoangnguyenhung1998 <hunglatoi000vn@gmail.com>
2018-05-05 17:18:47 +0700

    update 10 5/5 17:18

--- 1.js
+++ 1.js
@@ -1,4 +1,15 @@
 ﻿ $(function(){
- 
+ vitriedu=$('#khoiedu').offset().top;
+ $(".xuong,.history").click(function(){
+ 	$('body,html').animate({scrollTop:vitriedu},800)
+ })
+ vitrimenutop=$('#khoimenutop').offset().top;
+ $(".myself").click(function(){
+ 	$('body,html').animate({scrollTop:vitrimenutop},800)
+ }) 
+ vitrisp=$('#khoisp').offset().top;
+ $(".portfolio").click(function(){
+ 	$('body,html').animate({scrollTop:vitrisp},800)
+ }) 
 })  
  
